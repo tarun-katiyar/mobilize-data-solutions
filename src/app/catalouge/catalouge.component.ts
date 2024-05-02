5101ff9a83ef0f827387194d18afd4c11994d4a6
Tarun Katiyar <tarunkatiyar777@gmail.com>
2024-05-02 18:01:18 +0530

    add class side bar mini

--- src/app/catalouge/catalouge.component.ts
+++ src/app/catalouge/catalouge.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from '../../shared.service';
 
 @Component({
   selector: 'app-catalouge',
   templateUrl: './catalouge.component.html',
   styleUrl: './catalouge.component.css'
 })
-export class CatalougeComponent {
+export class CatalougeComponent implements OnInit {
+
+  isCatalougePage: boolean = false;
+  constructor(private _sharedService: SharedService) {
+
+  }
+
+  ngOnInit() {
+    let url = this._sharedService.getCurrentRoute();
+    if (url.includes('catalouge')) {
+      this.isCatalougePage = true;
+    }
+
+  }
 
 }
